0b1891447b8a432624c951341fd4690b44c1d627
mguzman08 <mguzman08@unisalle.edu.co>
2026-04-28 21:43:11 -0500

    Star creating the BPMN for our project

--- Pyhton/Tuplaslistas.py
+++ Pyhton/Tuplaslistas.py
@@ -6,3 +6,18 @@
 
 #diccionario = ("clave": 10, "valor": 5)
 
+
+# Definimos la función
+def calcular_imc(peso, altura):
+    resultado = peso / (altura ** 2)
+
+
+# Usamos la función en el programa principal
+mi_peso = 70
+mi_altura = 1.75
+
+# Guardamos lo que el return nos devuelve en una variable
+indice = calcular_imc(mi_peso, mi_altura)
+
+print(f"Tu IMC es: {indice:.2f}")
+
